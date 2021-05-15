987a27e45f1ef03d7068d0f2d996eb2ec17bedb2
Stenio Dias Rapchan <60396052+steniodr@users.noreply.github.com>
2021-05-15 17:09:06 -0300

    Create Exercicio-08.c

---
+++ Exercicio-08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float value, rate, income, final;
+
+  printf("Digite o valor do depósito: ");
+  scanf("%f", &value);
+  
+  printf("Digite a taxa de juros (Valor em \%): ");
+  scanf("%f", &rate);
+  
+  income = value * rate / 100;
+  final = value + income;
+  
+  printf("Valor Depositado: %.2f\n", value);
+  printf("Valor de Rendimento: %.2f\n", income);
+  printf("Valor Total: %.2f\n", final);
+  
+  return 0;
+}
